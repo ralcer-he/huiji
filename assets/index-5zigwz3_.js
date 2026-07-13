11c2b0fa5de1f75d32378c521f66cfa66cb1ab18
ralcer-he <2487054344@qq.com>
2026-07-14 02:36:59 +0800

    deploy: v1.06.4 相对路径修复

---
+++ assets/index-5zigwz3_.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-ZHn7WiyY.js","./main-BmvYSPjM.js","./main-BfnMZBXf.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./main-BmvYSPjM.js";const _=r("Share",{web:()=>t(()=>import("./web-ZHn7WiyY.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.ShareWeb)});export{_ as Share};
